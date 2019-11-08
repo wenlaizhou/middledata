cc6650118ba8ad1bbc8c32c398dc14d4ffa38ded
zhouwl <zhouwl@missfress.cn>
2019-11-08 18:49:55 +0800

    系统创建

--- framework.go
+++ framework.go
@@ -1,10 +1,13 @@
-package middledata
+package main
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/wenlaizhou/middleware"
+	_ "net/http/pprof"
+	"strings"
 	"time"
 )
 
@@ -12,9 +15,7 @@ func init() {
 
 }
 
-type Row map[string]interface{}
-
-const DSN = "%s:%s@tcp(%s:%s)/%s"
+const DSN = "%s:%s@tcp(%s)/%s"
 
 var Config middleware.Config
 
@@ -29,25 +30,67 @@ func Start(configPath string) {
 		return
 	}
 
+	logger.Info(middleware.ConfPrint(Config))
+
 	dsn := fmt.Sprintf(DSN,
 		middleware.ConfUnsafe(Config, "username"),
 		middleware.ConfUnsafe(Config, "password"),
 		middleware.ConfUnsafe(Config, "host"),
-		middleware.ConfUnsafe(Config, "port"),
 		middleware.ConfUnsafe(Config, "database"),
 	)
 	DB, err := sql.Open(middleware.ConfUnsafe(Config, "driver"), dsn)
 	if err != nil {
-		logger.Error(fmt.Sprintf("Open mysql failed,err:%v\n", err))
+		logger.Error(fmt.Sprintf("Open mysql failed,err:%v", err))
 		return
 	}
 	DB.SetConnMaxLifetime(100 * time.Second) // 最大连接周期，超过时间的连接就close
 	DB.SetMaxOpenConns(10)                   // 设置最大连接数
 	DB.SetMaxIdleConns(2)                    // 设置闲置连接数
 
-	middleware.RegisterHandler("metrics", func(context middleware.Context) {
+	SqlConf := map[string]interface{}{}
 
+	err = json.Unmarshal([]byte(middleware.ReadString(middleware.ConfUnsafe(Config, "sql"))), &SqlConf)
+	if err != nil {
+		logger.Error(fmt.Sprintf("sql conf error : %v;", err))
+		return
+	}
+
+	middleware.RegisterHandler("metrics", func(context middleware.Context) {
+		var res []string
+		for k, v := range SqlConf {
+			data := v.(map[string]interface{})
+			rows, err := DB.Query(fmt.Sprintf("%v", data["sql"]))
+			if err != nil {
+				logger.ErrorF("%v", err)
+			}
+			value := ""
+			if rows.Next() {
+				var x []uint8
+				err = rows.Scan(&x)
+				value = fmt.Sprintf("%s", x)
+			}
+			metricsData := middleware.MetricsData{
+				Key:   k,
+				Value: value,
+				Tags:  map[string]string{},
+			}
+			for k, v := range data {
+				if k == "sql" {
+					continue
+				}
+				metricsData.Tags[k] = fmt.Sprintf("%v", v)
+			}
+			res = append(res, middleware.FormatMetricsData(metricsData))
+		}
+
+		context.OK("text/plain", []byte(strings.Join(res, "\n")))
 	})
 
 	middleware.StartServer("0.0.0.0", middleware.ConfIntUnsafe(Config, "port"))
+}
+
+func main() {
+
+	Start("app.properties")
+
 }
